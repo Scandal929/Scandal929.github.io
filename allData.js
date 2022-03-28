b9387cc2ce3936da9dbc2035f9b11806e2b1f04b
Scandal929 <84350659+Scandal929@users.noreply.github.com>
2022-03-28 18:33:49 -0400

    Add files via upload

--- allData.js
+++ allData.js
@@ -1122,7 +1122,7 @@ var allData = [
     "state": "Vermont",
     "region": "New England",
     "city": "Fayston (ski only)",
-    "ski only": true,
+    "skiOnly": true,
     "videos": [],
     "position": {
       "lat": 44.202645696018386,
@@ -5356,7 +5356,7 @@ var allData = [
     "resortName": "Alta",
     "state": "Utah",
     "region": "Rocky Mountains",
-    "city": "Alta, (ski only)",
+    "city": "Alta (ski only)",
     "skiOnly": true,
     "videos": [],
     "position": {
