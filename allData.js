9e691931f079d4025c59e69fe7e855df8df8fcbf
Scandal929 <84350659+Scandal929@users.noreply.github.com>
2022-03-29 12:17:31 -0400

    Add files via upload

--- allData.js
+++ allData.js
@@ -1953,7 +1953,7 @@ var allData = [
     "state": "Pennsylvania",
     "region": "Mid-Atlantic",
     "city": "Tannersville",
-    "videos": ["xptv5RSx5Ds"],
+    "videos": ["xptv5RSx5Ds","YsHsp0HTONg"],
     "position": {
       "lat": 41.051811334929965,
       "lng": -75.35550284495609
